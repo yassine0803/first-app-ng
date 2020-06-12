7c5d6deaceacbaa894736f7bdbdbbd9d9e68a59b
yassine aourhebal <aourhebal.yassine@gmail.com>
2020-06-12 16:59:46 +0100

    property binding created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'mon-premier-projet';
+  isAuth = false;
+
+  constructor() {
+   setTimeout(
+    () => {
+      this.isAuth = true;
+    }, 4000
+   );
+  }
 }
