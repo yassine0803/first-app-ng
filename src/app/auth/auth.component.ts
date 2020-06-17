bf62d7ac71a396c1837cda99ff3ae9bf047929cf
yassine aourhebal <aourhebal.yassine@gmail.com>
2020-06-17 13:28:01 +0100

    authService created

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-auth',
@@ -7,9 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AuthComponent implements OnInit {
 
-  constructor() { }
+  authStatus: boolean;
+
+  constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
+    this.authStatus = this.authService.isAuth;
   }
-
+  onSignIn(){
+    this.authService.signIn().then(
+      () => {
+        console.log("connexion réussie ! ");
+        this.authStatus = this.authService.isAuth;
+        console.log(this.authStatus);
+      }
+    );
+  }
+  onSignOut(){
+    this.authService.signOut();
+    console.log("déconnexion réussie ! ");
+    this.authStatus = this.authService.isAuth;
+    console.log("authStatus "+this.authStatus);
+  }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -13,6 +13,6 @@ export class AuthService{
         );
     }
     signOut(){
-        this.isAuth = true;
+        this.isAuth = false;
     }
 }
