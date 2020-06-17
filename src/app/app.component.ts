2b59c4f0f45c052387207a702685802c43ea4653
yassine aourhebal <aourhebal.yassine@gmail.com>
2020-06-17 16:37:19 +0100

    observable.interval created in AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/Rx';
+import { Subscription } from 'rxjs/Rx';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'mon-premier-projet';
-  
+export class AppComponent implements OnInit, OnDestroy{
+  secondes: number;
+  counterSubscription: Subscription;
+  constructor(){}
+  ngOnInit(){
+    const counter = Observable.interval(1000);
+    counter.subscribe(
+      (value: number)=>{
+        this.secondes = value;
+      }
+    );
+  }
+  ngOnDestroy(){
+    this.counterSubscription.unsubscribe();
+  }
 }
