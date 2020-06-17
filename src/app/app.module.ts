2d2ef3c4add384d022219983c6f871c19e475862
yassine aourhebal <aourhebal.yassine@gmail.com>
2020-06-17 12:38:08 +0100

    AuthService created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { AppareilService }   from './services/appareil.services';
 import { AuthComponent } from './auth/auth.component';
 import { AppareilViewComponent } from './appareil-view/appareil-view.component'
 import { Routes, RouterModule } from '@angular/router';
+import { AuthService } from './services/auth.service';
 
 const appRoutes: Routes = [
   {path: 'appareils', component: AppareilViewComponent},
@@ -31,7 +32,8 @@ const appRoutes: Routes = [
     RouterModule.forRoot(appRoutes)
   ],
   providers: [
-    AppareilService
+    AppareilService,
+    AuthService
   ],
   bootstrap: [AppComponent]
 })
