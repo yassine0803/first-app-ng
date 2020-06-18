90a56553aa202ff0a09251fe3a86bb9462092b22
yassine aourhebal <aourhebal.yassine@gmail.com>
2020-06-18 14:20:33 +0100

    fomrArray created

--- src/app/new-user/new-user.component.ts
+++ src/app/new-user/new-user.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validator, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
 import { UserService } from '../services/user.service';
 import { Router } from '@angular/router';
 import { User } from '../models/user.model';
@@ -13,31 +13,39 @@ export class NewUserComponent implements OnInit {
 
   userForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder, private userService: UserService,private router:Router) { }
+  constructor(private formBuilder: FormBuilder, private userService: UserService, private router: Router) { }
 
   ngOnInit(): void {
     this.initForm();
   }
-  initForm(){
+  initForm() {
     this.userForm = this.formBuilder.group(
       {
         firstName: ['', Validators.required],
-        lastName:['', Validators.required],
-        email:['', [Validators.required, Validators.email]],
-        drinkPreference:['', Validators.required]
+        lastName: ['', Validators.required],
+        email: ['', [Validators.required, Validators.email]],
+        drinkPreference: ['', Validators.required],
+        hobbies: this.formBuilder.array([])
       }
     );
   }
-  onSubmitForm(){
+  onSubmitForm() {
     const formValue = this.userForm.value;
     const newUser = new User(
       formValue['firstName'],
       formValue['lastName'],
       formValue['email'],
-      formValue['drinkPreference']
+      formValue['drinkPreference'],
+      formValue['hobbies'] ? formValue['hobbies'] : []
     );
     this.userService.addUser(newUser);
     this.router.navigate(['/users']);
   }
-
+  getHobbies() {
+    return this.userForm.get('hobbies') as FormArray;
+  }
+  onAddHobby() {
+    const newHobbyControl = this.formBuilder.control(null, Validators.required);
+    this.getHobbies().push(newHobbyControl);
+  }
 }
