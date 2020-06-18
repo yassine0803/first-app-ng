67d1a9d63539e46caebcb22c5711d58fbb68b321
yassine aourhebal <aourhebal.yassine@gmail.com>
2020-06-18 13:37:07 +0100

    formUser created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -17,6 +17,7 @@ import { AuthGuard } from './services/auth-guard.service';
 import { EditAppareilComponent } from './edit-appareil/edit-appareil.component';
 import { UserListComponent } from './user-list/user-list.component';
 import { UserService } from './services/user.service';
+import { NewUserComponent } from './new-user/new-user.component';
 
 const appRoutes: Routes = [
   {path: 'appareils', canActivate: [AuthGuard], component: AppareilViewComponent},
@@ -38,12 +39,14 @@ const appRoutes: Routes = [
     AppareilViewComponent,
     FourOhFourComponent,
     EditAppareilComponent,
-    UserListComponent
+    UserListComponent,
+    NewUserComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    ReactiveFormsModule,
     RouterModule.forRoot(appRoutes)
   ],
   providers: [
---
+++ src/app/new-user/new-user.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { UserService } from '../services/user.service';
+import { Router } from '@angular/router';
+import { User } from '../models/user.model';
+
+@Component({
+  selector: 'app-new-user',
+  templateUrl: './new-user.component.html',
+  styleUrls: ['./new-user.component.scss']
+})
+export class NewUserComponent implements OnInit {
+
+  userForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder, private userService: UserService,private router:Router) { }
+
+  ngOnInit(): void {
+    this.initForm();
+  }
+  initForm(){
+    this.userForm = this.formBuilder.group(
+      {
+        firstName: '',
+        lastName:'',
+        email:'',
+        drinkPrefence:''
+      }
+    );
+  }
+  onSubmitForm(){
+    const formValue = this.userForm.value;
+    const newUser = new User(
+      formValue['firstName'],
+      formValue['lastName'],
+      formValue['email'],
+      formValue['drinkPreference']
+    );
+    this.userService.addUser(newUser);
+    this.router.navigate(['/users']);
+  }
+
+}
